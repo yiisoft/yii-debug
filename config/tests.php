4b2d41b7728210edf8ba2553643fba459eb33607
Dmitriy Derepko <xepozz@list.ru>
2020-02-15 16:15:57 +0300

    Implemented LogCollector

--- config/tests.php
+++ config/tests.php
@@ -1,4 +1,11 @@
 <?php
 
 return [
+    \Yiisoft\Yii\Debug\Target\TargetInterface::class=>\Yiisoft\Yii\Debug\Target\MemTarget::class,
+    \Psr\Log\LoggerInterface::class => function (\Psr\Container\ContainerInterface $container) {
+        return new \Yiisoft\Yii\Debug\Collector\LogCollector(
+            $container->get(\Yiisoft\Yii\Debug\Target\TargetInterface::class),
+            $container->get(\Yiisoft\Log\Logger::class)
+        );
+    },
 ];
