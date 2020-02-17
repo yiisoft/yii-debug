0c09dbfad92119a443061778f9b62d429f7c3b31
Dmitriy Derepko <xepozz@list.ru>
2020-02-17 21:33:35 +0300

    Added main class with overriding container variable

---
+++ src/Debug.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Yiisoft\Yii\Debug;
+
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\Log\LoggerInterface;
+use Yiisoft\Di\CompositeContainer;
+use Yiisoft\Di\Container;
+use Yiisoft\EventDispatcher\Dispatcher;
+use Yiisoft\EventDispatcher\Provider\Provider;
+use Yiisoft\Yii\Debug\Collector\EventCollector;
+use Yiisoft\Yii\Debug\Collector\LogCollector;
+use Yiisoft\Yii\Debug\Collector\RequestCollector;
+use Yiisoft\Yii\Debug\Target\MemTarget;
+use Yiisoft\Yii\Debug\Target\TargetInterface;
+
+class Debug
+{
+    public static function init(ContainerInterface &$container): void
+    {
+        $definitions = self::getDefinitions($container);
+        $debugContainer = new Container($definitions);
+
+        $container = new CompositeContainer();
+        $container->attach($debugContainer);
+        $container->attach($container);
+    }
+
+    private static function getDefinitions(ContainerInterface $originContainer): array
+    {
+        return [
+            TargetInterface::class => MemTarget::class,
+            LoggerInterface::class => function () use ($originContainer) {
+                return new LogCollector($originContainer->get(LoggerInterface::class));
+            },
+            EventDispatcherInterface::class => function () use ($originContainer) {
+                return new EventCollector($originContainer->get(Dispatcher::class));
+            },
+            ListenerProviderInterface::class => function () use ($originContainer) {
+                return new RequestCollector($originContainer->get(Provider::class));
+            },
+        ];
+    }
+}
