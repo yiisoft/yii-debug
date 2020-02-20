7993a340313c9552306eb86913c618165eaccda7
Dmitriy Derepko <xepozz@list.ru>
2020-02-20 15:25:21 +0300

    Removed DummyCollector

--- tests/Collector/DummyCollectorTest.php
+++ tests/Collector/DummyCollectorTest.php
@@ -3,12 +3,31 @@
 namespace Yiisoft\Yii\Debug\Tests\Collector;
 
 use Yiisoft\Yii\Debug\Collector\CollectorInterface;
-use Yiisoft\Yii\Debug\Collector\DummyCollector;
 
 class DummyCollectorTest extends AbstractCollectorTestCase
 {
     protected function getCollector(): CollectorInterface
     {
         return new DummyCollector();
+    }
+}
+
+final class DummyCollector implements CollectorInterface
+{
+    public function collect(): array
+    {
+        return [
+            'int' => 123,
+            'str' => 'asdas',
+            'object' => new \stdClass(),
+        ];
+    }
+
+    public function startup(): void
+    {
+    }
+
+    public function shutdown(): void
+    {
     }
 }
