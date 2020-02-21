55e134168eed3469d885b9657000541885d43c8f
Dmitriy Derepko <xepozz@list.ru>
2020-02-21 21:22:50 +0300

    Moved configuration to config file

--- config/web.php
+++ config/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Psr\Container\ContainerInterface;
+use Yiisoft\Aliases\Aliases;
 use Yiisoft\Yii\Debug\Debugger;
+use Yiisoft\Yii\Debug\Target\FileTarget;
 use Yiisoft\Yii\Debug\Target\TargetInterface;
 
 /**
@@ -9,6 +11,12 @@ use Yiisoft\Yii\Debug\Target\TargetInterface;
  */
 
 return [
+    TargetInterface::class => function (ContainerInterface $container) {
+        $runtime = $container->get(Aliases::class)->get('@runtime');
+        $id = time();
+
+        return new FileTarget("$runtime/debug/$id.data");
+    },
     Debugger::class => function (ContainerInterface $container) use ($params) {
         return new Debugger(
             $container->get(TargetInterface::class),
