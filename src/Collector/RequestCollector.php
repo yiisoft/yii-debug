41c4274df0a47c02eda2d6fb786c3bf79ea8da23
Dmitriy Derepko <xepozz@list.ru>
2020-02-21 20:20:43 +0300

    Clean

--- src/Collector/RequestCollector.php
+++ src/Collector/RequestCollector.php
@@ -3,10 +3,6 @@
 namespace Yiisoft\Yii\Debug\Collector;
 
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Yiisoft\Yii\Debug\Event\RequestEndEvent;
-use Yiisoft\Yii\Debug\Event\RequestStartedEvent;
 use Yiisoft\Yii\Web\Event\AfterRequest;
 use Yiisoft\Yii\Web\Event\ApplicationShutdown;
 use Yiisoft\Yii\Web\Event\ApplicationStartup;
@@ -16,24 +12,14 @@ class RequestCollector implements CollectorInterface, EventDispatcherInterface
 {
     use CollectorTrait;
 
-    private ?ServerRequestInterface $request = null;
-    private ?ResponseInterface $response = null;
-    private EventDispatcherInterface $eventDispatcher;
     private float $applicationProcessingTimeStarted = 0;
     private float $applicationProcessingTimeStopped = 0;
     private float $requestProcessingTimeStarted = 0;
     private float $requestProcessingTimeStopped = 0;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
-        $this->eventDispatcher = $eventDispatcher;
-    }
-
     public function collect(): array
     {
         return [
-            $this->request,
-            $this->response,
             'application_processing_time' => $this->applicationProcessingTimeStopped - $this->applicationProcessingTimeStarted,
             'request_processing_time' => $this->requestProcessingTimeStopped - $this->requestProcessingTimeStarted,
             'memory_peak_usage' => memory_get_peak_usage(true),
@@ -48,15 +34,13 @@ class RequestCollector implements CollectorInterface, EventDispatcherInterface
                 $this->requestProcessingTimeStarted = microtime(true);
             } elseif ($event instanceof AfterRequest) {
                 $this->requestProcessingTimeStopped = microtime(true);
+            } elseif ($event instanceof ApplicationStartup) {
+                $this->applicationProcessingTimeStarted = microtime(true);
+            } elseif ($event instanceof ApplicationShutdown) {
+                $this->applicationProcessingTimeStopped = microtime(true);
             }
         }
 
-        if ($event instanceof ApplicationStartup) {
-            $this->applicationProcessingTimeStarted = microtime(true);
-        } elseif ($event instanceof ApplicationShutdown) {
-            $this->applicationProcessingTimeStopped = microtime(true);
-        }
-
-        return $this->eventDispatcher->dispatch($event);
+        return $event;
     }
 }
