9f5013b2ac7acd2a0009a30415b4234b9b92d0ef
Dmitriy Derepko <xepozz@list.ru>
2020-02-21 18:32:51 +0300

    Refactored

--- src/DebugServiceProvider.php
+++ src/DebugServiceProvider.php
@@ -4,12 +4,12 @@ namespace Yiisoft\Yii\Debug;
 
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
 use Psr\Log\LoggerInterface;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\Contracts\ServiceProviderInterface;
-use Yiisoft\EventDispatcher\Provider\CompositeProvider;
+use Yiisoft\EventDispatcher\Dispatcher\CompositeDispatcher;
+use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
 use Yiisoft\Yii\Debug\Collector\EventCollector;
 use Yiisoft\Yii\Debug\Collector\LogCollector;
 use Yiisoft\Yii\Debug\Collector\RequestCollector;
@@ -22,7 +22,6 @@ class DebugServiceProvider implements ServiceProviderInterface
     {
         $logger = $container->get(LoggerInterface::class);
         $dispatcher = $container->get(EventDispatcherInterface::class);
-        $listenerProvider = $container->get(ListenerProviderInterface::class);
 
         $container->setMultiple(
             [
@@ -32,31 +31,27 @@ class DebugServiceProvider implements ServiceProviderInterface
 
                     return new FileTarget("$runtime/debug/$id.data");
                 },
-                LoggerInterface::class => function (ContainerInterface $container) use ($logger) {
-                    return new LogCollector($logger);
+                LogCollector::class => fn() => new LogCollector($logger),
+                LoggerInterface::class => LogCollector::class,
+                RequestCollector::class => function (ContainerInterface $container) {
+                    return new RequestCollector(
+                        new Dispatcher($container->get(DebugListenerProvider::class))
+                    );
                 },
-                EventDispatcherInterface::class => function (ContainerInterface $container) use ($dispatcher) {
-                    $compositeDispatcher = new \Yiisoft\EventDispatcher\Dispatcher\CompositeDispatcher();
-                    $compositeDispatcher->attach(new \Yiisoft\EventDispatcher\Dispatcher\Dispatcher($container->get(DebugListenerProvider::class)));
+                EventCollector::class => function (ContainerInterface $container) use ($dispatcher) {
+                    $compositeDispatcher = new CompositeDispatcher();
+                    $compositeDispatcher->attach($container->get(RequestCollector::class));
                     $compositeDispatcher->attach($dispatcher);
 
                     return new EventCollector($compositeDispatcher);
                 },
-                ListenerProviderInterface::class => function (ContainerInterface $container) use ($listenerProvider) {
-                    $provider = new CompositeProvider();
-                    $provider->attach($listenerProvider);
-                    $provider->attach($container->get(DebugListenerProvider::class));
-
-                    return new RequestCollector($provider);
-                },
+                EventDispatcherInterface::class => EventCollector::class,
                 Debugger::class => function (ContainerInterface $container) {
                     return new Debugger(
                         $container->get(TargetInterface::class),
-                        ...[
-                            $container->get(LoggerInterface::class),
-                            $container->get(EventDispatcherInterface::class),
-                            $container->get(ListenerProviderInterface::class),
-                        ]
+                        $container->get(LogCollector::class),
+                        $container->get(EventCollector::class),
+                        $container->get(RequestCollector::class),
                     );
                 },
             ]
