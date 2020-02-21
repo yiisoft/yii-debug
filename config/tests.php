3eadaf396a0b967d870fedf801f5b9b06b30f725
Dmitriy Derepko <xepozz@list.ru>
2020-02-21 19:56:14 +0300

    Clean config

--- config/tests.php
+++ config/tests.php
@@ -2,35 +2,20 @@
 
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
 use Psr\Log\LoggerInterface;
 use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
-use Yiisoft\EventDispatcher\Provider\Provider;
 use Yiisoft\Log\Logger;
 use Yiisoft\Yii\Debug\Collector\EventCollector;
 use Yiisoft\Yii\Debug\Collector\LogCollector;
-use Yiisoft\Yii\Debug\Collector\RequestCollector;
 use Yiisoft\Yii\Debug\Target\MemTarget;
 use Yiisoft\Yii\Debug\Target\TargetInterface;
 
 return [
     TargetInterface::class => MemTarget::class,
     LoggerInterface::class => function (ContainerInterface $container) {
-        return new LogCollector(
-            $container->get(Logger::class)
-        );
+        return new LogCollector($container->get(Logger::class));
     },
     EventDispatcherInterface::class => function (ContainerInterface $container) {
-        return new EventCollector(
-            $container->get(Dispatcher::class)
-        );
-    },
-    ListenerProviderInterface::class => function (ContainerInterface $container) {
-        return new RequestCollector(
-            $container->get(Dispatcher::class)
-        );
-    },
-    Dispatcher::class => function () {
-        return new Yiisoft\EventDispatcher\Dispatcher\Dispatcher(new Provider());
+        return new EventCollector($container->get(Dispatcher::class));
     },
 ];
