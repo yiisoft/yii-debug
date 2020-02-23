443080a98630d9e875381a0000b4521bb421ba51
Dmitriy Derepko <xepozz@list.ru>
2020-02-23 12:38:03 +0300

    Clean tests

--- tests/Collector/AbstractCollectorTestCase.php
+++ tests/Collector/AbstractCollectorTestCase.php
@@ -2,44 +2,18 @@
 
 namespace Yiisoft\Yii\Debug\Tests\Collector;
 
-use hiqdev\composer\config\Builder;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
-use Yiisoft\Di\Container;
 use Yiisoft\Yii\Debug\Collector\CollectorInterface;
-use Yiisoft\Yii\Debug\Target\MemTarget;
-use Yiisoft\Yii\Debug\Target\TargetInterface;
 
 abstract class AbstractCollectorTestCase extends TestCase
 {
-    protected ContainerInterface $container;
-
-    protected function setUp(): void
-    {
-        $config = require Builder::path('tests');
-
-        $this->container = new Container($config);
-    }
-
-    /**
-     * @dataProvider targetProvider()
-     * @param \Yiisoft\Yii\Debug\Target\TargetInterface $target
-     */
-    public function testExport(TargetInterface $target): void
+    public function testExport(): void
     {
         $collector = $this->getCollector();
         $collector->startup();
-        $this->assertEmpty($target->getData());
         $this->somethingDoTestExport($collector);
         $this->assertExportedData($collector);
         $collector->shutdown();
-    }
-
-    public function targetProvider(): array
-    {
-        return [
-            [new MemTarget()],
-        ];
     }
 
     abstract protected function getCollector(): CollectorInterface;
