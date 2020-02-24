fc6b13ff21fc3a8f31ca58e3cc4391092c70b9c9
Alexander Makarov <sam@rmcreative.ru>
2020-02-24 19:53:26 +0300

    Add phpdoc to CollectorInterface

--- src/Collector/CollectorInterface.php
+++ src/Collector/CollectorInterface.php
@@ -2,13 +2,32 @@
 
 namespace Yiisoft\Yii\Debug\Collector;
 
+/**
+ * Debug data collector responsibility is to collect data during application lifecycle.
+ */
 interface CollectorInterface
 {
+    /**
+     * Called once at application startup.
+     * Any initialization could be done here.
+     */
     public function startup(): void;
 
+    /**
+     * Called once at application shutdown.
+     * Cleanup could be done here.
+     */
     public function shutdown(): void;
 
+    /**
+     * Collect data.
+     * @return array data collected
+     */
     public function collect(): array;
 
+    /**
+     * Dispatch data collected for further processing.
+     * @param mixed ...$payload
+     */
     public function dispatch(...$payload): void;
 }
