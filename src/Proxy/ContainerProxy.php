58ef422ac1b26d0ea75c4dfc35799ee6b07668b4
yiiliveext <yiiliveext@gmail.com>
2020-03-04 12:52:59 +0200

    Remove not puclic method from proxy

--- src/Proxy/ContainerProxy.php
+++ src/Proxy/ContainerProxy.php
@@ -3,60 +3,16 @@
 namespace Yiisoft\Yii\Debug\Proxy;
 
 use Psr\Container\ContainerInterface;
-use Psr\Container\ContainerExceptionInterface;
 use Yiisoft\Di\Container;
-use Yiisoft\Di\ContainerDelegateInterface;
 
-final class ContainerProxy extends ContainerInterfaceProxy implements ContainerDelegateInterface
+final class ContainerProxy extends ContainerInterfaceProxy
 {
     public function __construct(
         ContainerInterface $container,
         ContainerProxyConfig $config
     ) {
-        $container = $container instanceof ContainerDelegateInterface ? $container->delegateLookup($this) : $container;
+        $container = $container instanceof Container ? $container->delegateLookup($this) : $container;
         parent::__construct($container, $config);
-    }
-
-    public function set(string $id, $definition): void
-    {
-        $this->checkNativeContainer();
-        $this->resetCurrentError();
-        $timeStart = microtime(true);
-        try {
-            $this->container->set($id, $definition);
-        } catch (ContainerExceptionInterface $e) {
-            $this->repeatError($e);
-        } finally {
-            $this->log('set', [$id, $definition], null, $timeStart);
-        }
-    }
-
-    public function setMultiple(array $config): void
-    {
-        $this->checkNativeContainer();
-        $this->resetCurrentError();
-        $timeStart = microtime(true);
-        try {
-            $this->container->setMultiple($config);
-        } catch (ContainerExceptionInterface $e) {
-            $this->repeatError($e);
-        } finally {
-            $this->log('setMultiple', [$config], null, $timeStart);
-        }
-    }
-
-    public function addProvider($providerDefinition): void
-    {
-        $this->checkNativeContainer();
-        $this->resetCurrentError();
-        $timeStart = microtime(true);
-        try {
-            $this->container->addProvider($providerDefinition);
-        } catch (ContainerExceptionInterface $e) {
-            $this->repeatError($e);
-        } finally {
-            $this->log('addProvider', [$providerDefinition], null, $timeStart);
-        }
     }
 
     public function delegateLookup(ContainerInterface $container): ContainerInterface
