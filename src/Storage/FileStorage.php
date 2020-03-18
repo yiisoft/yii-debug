b696b129ce6c572bf270fe2c50ed1afa5a56bae6
yiiliveext <yiiliveext@gmail.com>
2020-03-18 16:57:17 +0200

    Make collectors always clear after flush

--- src/Storage/FileStorage.php
+++ src/Storage/FileStorage.php
@@ -44,13 +44,15 @@ final class FileStorage implements StorageInterface
 
     public function flush(): void
     {
-        $varDumper = VarDumper::create($this->getData());
-        $jsonData = $varDumper->asJson();
-        $this->filesystem->write($this->path . '/' . $this->debugId . '.data.json', $jsonData);
+        try {
+            $varDumper = VarDumper::create($this->getData());
+            $jsonData = $varDumper->asJson();
+            $this->filesystem->write($this->path . '/' . $this->debugId . '.data.json', $jsonData);
 
-        $jsonObjects = $varDumper->asJsonObjectsMap();
-        $this->filesystem->write($this->path . '/' . $this->debugId . '.obj.json', $jsonObjects);
-
-        $this->collectors = [];
+            $jsonObjects = $varDumper->asJsonObjectsMap();
+            $this->filesystem->write($this->path . '/' . $this->debugId . '.obj.json', $jsonObjects);
+        } finally {
+            $this->collectors = [];
+        }
     }
 }
