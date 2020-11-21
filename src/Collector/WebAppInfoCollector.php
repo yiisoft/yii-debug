16b1b125657660fe5707584fcf78419a6d1a3c0a
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-11-21 08:58:01 +0500

    Fix

--- src/Collector/WebAppInfoCollector.php
+++ src/Collector/WebAppInfoCollector.php
@@ -61,7 +61,7 @@ final class WebAppInfoCollector implements CollectorInterface, IndexCollectorInt
         return [
             'time' => $this->requestProcessingTimeStopped - $this->requestProcessingTimeStarted,
             'memory' => memory_get_peak_usage(),
-            'timestamp' => $this->requestProcessingTimeStarted
+            'timestamp' => $this->requestProcessingTimeStarted,
         ];
     }
 }
