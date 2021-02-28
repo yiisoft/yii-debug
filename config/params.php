5dfda403ec19f49d2d757ff5fd0dd4fc2be06e3c
Rustam Mamadaminov <rmamdaminov@gmail.com>
2021-02-28 10:47:43 +0500

    Add PSR Cache

--- config/params.php
+++ config/params.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Log\LoggerInterface;
-use Yiisoft\Cache\CacheInterface;
+use Psr\SimpleCache\CacheInterface;
+use Yiisoft\Cache\CacheInterface as YiiCacheInterface;
 use Yiisoft\Router\UrlMatcherInterface;
 use Yiisoft\Yii\Debug\Collector\CommandCollector;
 use Yiisoft\Yii\Debug\Collector\ConsoleAppInfoCollector;
@@ -47,6 +48,7 @@ return [
             LoggerInterface::class => [LoggerInterfaceProxy::class, LogCollectorInterface::class],
             EventDispatcherInterface::class => [EventDispatcherInterfaceProxy::class, EventCollectorInterface::class],
             UrlMatcherInterface::class => [UrlMatcherInterfaceProxy::class, RouterCollectorInterface::class],
+            YiiCacheInterface::class,
             CacheInterface::class,
         ],
         'logLevel' => ContainerProxy::LOG_ARGUMENTS | ContainerProxy::LOG_RESULT | ContainerProxy::LOG_ERROR,
