d5f621947abf80b412992e7d99926b3d547a3590
yiiliveext <yiiliveext@gmail.com>
2021-08-06 19:44:00 +0300

    New providers (#100)

--- src/DebugServiceProvider.php
+++ src/DebugServiceProvider.php
@@ -7,33 +7,29 @@ namespace Yiisoft\Yii\Debug;
 use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Log\LoggerInterface;
-use Yiisoft\Di\Container;
-use Yiisoft\Di\Support\ServiceProvider;
+use Yiisoft\Di\Contracts\ServiceProviderInterface;
 use Yiisoft\Yii\Debug\Collector\EventCollector;
 use Yiisoft\Yii\Debug\Collector\LogCollector;
 use Yiisoft\Yii\Debug\Proxy\EventDispatcherInterfaceProxy;
 use Yiisoft\Yii\Debug\Proxy\LoggerInterfaceProxy;
 
-final class DebugServiceProvider extends ServiceProvider
+final class DebugServiceProvider implements ServiceProviderInterface
 {
-    public function register(Container $container): void
-    {
-        $logger = $container->get(LoggerInterface::class);
-        $dispatcher = $container->get(EventDispatcherInterface::class);
 
-        /**
-         * @psalm-suppress InaccessibleMethod
-         */
-        $container->setMultiple(
-            [
-                // interfaces overriding
-                LoggerInterface::class => static function (ContainerInterface $container) use ($logger) {
-                    return new LoggerInterfaceProxy($logger, $container->get(LogCollector::class));
-                },
-                EventDispatcherInterface::class => static function (ContainerInterface $container) use ($dispatcher) {
-                    return new EventDispatcherInterfaceProxy($dispatcher, $container->get(EventCollector::class));
-                },
-            ]
-        );
+    public function getDefinitions(): array
+    {
+        return [];
+    }
+
+    public function getExtensions(): array
+    {
+        return [
+            LoggerInterface::class => static function (ContainerInterface $container, LoggerInterface $logger) {
+                return new LoggerInterfaceProxy($logger, $container->get(LogCollector::class));
+            },
+            EventDispatcherInterface::class => static function (ContainerInterface $container, EventDispatcherInterface $dispatcher) {
+                return new EventDispatcherInterfaceProxy($dispatcher, $container->get(EventCollector::class));
+            },
+        ];
     }
 }
