9151e66de4d9e9bad473ceb094e2f87768271b1e
Rustam <rmamdaminov@gmail.com>
2021-08-22 23:11:26 +0500

    Fix config

--- config/web.php
+++ config/web.php
@@ -2,24 +2,23 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-use Yiisoft\Di\DynamicReferencesArray;
+use Yiisoft\Di\ReferencesArray;
 use Yiisoft\Yii\Debug\Debugger;
-use Yiisoft\Yii\Debug\DebuggerIdGenerator;
-use Yiisoft\Yii\Debug\Storage\StorageInterface;
 
 if (!(bool)($params['yiisoft/yii-debug']['enabled'] ?? false)) {
     return [];
 }
 
 return [
-    Debugger::class => static function (ContainerInterface $container) use ($params) {
-        $params = $params['yiisoft/yii-debug'];
-        return new Debugger(
-            $container->get(DebuggerIdGenerator::class),
-            $container->get(StorageInterface::class),
-            DynamicReferencesArray::from(array_merge($params['collectors'], $params['collectors.web'] ?? [])),
-            $params['optionalRequests']
-        );
-    },
+    Debugger::class => [
+        '__construct()' => [
+            'collectors' => ReferencesArray::from(
+                array_merge(
+                    $params['yiisoft/yii-debug']['collectors'],
+                    $params['yiisoft/yii-debug']['collectors.web'] ?? []
+                )
+            ),
+            'optionalRequests' => $params['yiisoft/yii-debug']['optionalRequests'],
+        ],
+    ],
 ];
