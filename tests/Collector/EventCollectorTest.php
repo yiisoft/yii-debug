2d66b98b37a13d10b6ee1b4c451e806f014335a2
Rustam <rmamdaminov@gmail.com>
2022-03-25 19:26:18 +0500

    Add tests

--- tests/Collector/EventCollectorTest.php
+++ tests/Collector/EventCollectorTest.php
@@ -6,6 +6,7 @@ namespace Yiisoft\Yii\Debug\Tests\Collector;
 
 use Yiisoft\Yii\Debug\Collector\CollectorInterface;
 use Yiisoft\Yii\Debug\Collector\EventCollector;
+use Yiisoft\Yii\Debug\Tests\Support\DummyEvent;
 
 final class EventCollectorTest extends CollectorTestCase
 {
@@ -14,11 +15,17 @@ final class EventCollectorTest extends CollectorTestCase
      */
     protected function collectTestData(CollectorInterface $collector): void
     {
-        $collector->collect(new \stdClass(), __FILE__ . ':' . __LINE__);
+        $collector->collect(new DummyEvent(), __FILE__ . ':' . __LINE__);
     }
 
     protected function getCollector(): CollectorInterface
     {
         return new EventCollector();
+    }
+
+    protected function checkCollectedData(CollectorInterface $collector): void
+    {
+        parent::checkCollectedData($collector);
+        $this->assertFileExists($collector->getCollected()[0]['file']);
     }
 }
