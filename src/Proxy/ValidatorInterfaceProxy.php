13190d172f2436378847740d6c9532f39190f65b
Dmitrii Derepko <xepozz@list.ru>
2022-07-19 09:42:38 +0300

    Adopt https://github.com/yiisoft/validator/pull/255

--- src/Proxy/ValidatorInterfaceProxy.php
+++ src/Proxy/ValidatorInterfaceProxy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Debug\Proxy;
 
 use Yiisoft\Validator\Result;
+use Yiisoft\Validator\RulesProviderInterface;
 use Yiisoft\Validator\ValidatorInterface;
 use Yiisoft\Yii\Debug\Collector\ValidatorCollectorInterface;
 
@@ -19,6 +20,15 @@ final class ValidatorInterfaceProxy implements ValidatorInterface
     public function validate(mixed $data, iterable $rules = []): Result
     {
         $result = $this->validator->validate($data, $rules);
+
+        if ($data instanceof RulesProviderInterface) {
+            $explicitRules = $rules;
+            $rules = (array) $data->getRules();
+
+            foreach ($explicitRules as $key => $value) {
+                $rules[$key] = $value;
+            }
+        }
 
         $this->collector->collect(
             $data,
