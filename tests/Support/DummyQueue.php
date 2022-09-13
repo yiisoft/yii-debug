39a761faf7a8dbdf8e9a5bb24decef86907be2f6
Dmitrii Derepko <xepozz@list.ru>
2022-09-13 15:58:50 +0300

    Fix debugger

--- tests/Support/DummyQueue.php
+++ tests/Support/DummyQueue.php
@@ -8,6 +8,7 @@ use Exception;
 use Yiisoft\Yii\Queue\Adapter\AdapterInterface;
 use Yiisoft\Yii\Queue\Enum\JobStatus;
 use Yiisoft\Yii\Queue\Message\MessageInterface;
+use Yiisoft\Yii\Queue\Middleware\Push\MiddlewarePushInterface;
 use Yiisoft\Yii\Queue\QueueInterface;
 
 final class DummyQueue implements QueueInterface
@@ -16,8 +17,11 @@ final class DummyQueue implements QueueInterface
     {
     }
 
-    public function push(MessageInterface $message): void
-    {
+    public function push(
+        MessageInterface $message,
+        string|array|callable|MiddlewarePushInterface ...$middlewareDefinitions
+    ): MessageInterface {
+        return $message;
     }
 
     public function run(int $max = 0): void
