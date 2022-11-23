d4e072873f0a34dd03c2a3fa5ee63f2ed45be927
Rustam <rmamdaminov@gmail.com>
2022-11-23 19:44:03 +0500

    Fix tests

--- tests/DumperTest.php
+++ tests/DumperTest.php
@@ -82,12 +82,16 @@ final class DumperTest extends TestCase
         $staticShortFunctionObjectId = spl_object_id($staticShortFunctionObject);
 
         // @formatter:off
-        $functionObject = fn () => 1;
+        $functionObject = function () {
+            return 1;
+        };
         // @formatter:on
         $functionObjectId = spl_object_id($functionObject);
 
         // @formatter:off
-        $staticFunctionObject = static fn () => 1;
+        $staticFunctionObject = static function () {
+            return 1;
+        };;
         // @formatter:on
         $staticFunctionObjectId = spl_object_id($staticFunctionObject);
 
