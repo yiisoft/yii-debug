deccc4910c0b6555b91aeceede305759015582d1
Dmitrii Derepko <xepozz@list.ru>
2023-06-05 12:03:27 +0300

    Add tests

--- tests/Unit/Command/ResetCommandTest.php
+++ tests/Unit/Command/ResetCommandTest.php
@@ -6,17 +6,21 @@ namespace Yiisoft\Yii\Debug\Tests\Unit\Command;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
-use Yiisoft\Yii\Debug\Command\ResetCommand;
+use Yiisoft\Yii\Debug\Command\DebugResetCommand;
+use Yiisoft\Yii\Debug\Debugger;
+use Yiisoft\Yii\Debug\DebuggerIdGenerator;
 use Yiisoft\Yii\Debug\Storage\StorageInterface;
 
 final class ResetCommandTest extends TestCase
 {
     public function testCommand()
     {
+        $idGenerator = new DebuggerIdGenerator();
         $storage = $this->createMock(StorageInterface::class);
         $storage->expects($this->once())->method('clear');
+        $debugger = new Debugger($idGenerator, $storage, []);
 
-        $command = new ResetCommand($storage);
+        $command = new DebugResetCommand($storage, $debugger);
 
         $commandTester = new CommandTester($command);
 
