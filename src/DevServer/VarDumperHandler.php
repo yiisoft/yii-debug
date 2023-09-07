5c19d736866cb7ec0180f395e184c1591375bcc9
Dmitrii Derepko <xepozz@list.ru>
2023-09-07 19:33:32 +0300

    Add var-dumper handler for dev-server

---
+++ src/DevServer/VarDumperHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Debug\DevServer;
+
+use Yiisoft\VarDumper\HandlerInterface;
+
+final class VarDumperHandler implements HandlerInterface
+{
+    public Connection $connection;
+
+    public function __construct()
+    {
+        $this->connection = Connection::create();
+    }
+
+    public function handle(mixed $variable, int $depth, bool $highlight = false): void
+    {
+        $this->connection->broadcast(json_encode($variable));
+    }
+}
