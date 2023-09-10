becbf1e0408b14aba58a258a8ba83e1a27023e2f
Dmitrii Derepko <xepozz@list.ru>
2023-09-11 03:29:57 +0700

    Improve socket wrapper

--- src/DevServer/Connection.php
+++ src/DevServer/Connection.php
@@ -6,15 +6,26 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Debug\DevServer;
 
-use Closure;
+use Generator;
 use RuntimeException;
 use Socket;
 use Throwable;
 
+/**
+ * List of socket errors: {@see https://www.ibm.com/docs/en/zos/2.4.0?topic=calls-sockets-return-codes-errnos}
+ */
 final class Connection
 {
-    public const DEFAULT_SOCKET_DIR = '/tmp/var-dumper';
-    public const DEFAULT_SOCKET_URL = '/tmp/var-dumper-%d.sock';
+    public const DEFAULT_TIMEOUT = 10 * 1000; // 10 milliseconds
+    public const DEFAULT_BUFFER_SIZE = 1 * 1024; // 1 kilobyte
+
+    public const TYPE_RESULT = 0x001b;
+    public const TYPE_ERROR = 0x002b;
+
+    public const MESSAGE_TYPE_VAR_DUMPER = 0x001b;
+    public const MESSAGE_TYPE_LOGGER = 0x002b;
+
+    private string $uri;
 
     public function __construct(
         private Socket $socket,
@@ -25,6 +36,18 @@ final class Connection
     {
         $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
 
+        $socket_last_error = socket_last_error($socket);
+
+        if ($socket_last_error) {
+            throw new RuntimeException(
+                sprintf(
+                    '"socket_last_error" returned %d: "%s".',
+                    $socket_last_error,
+                    socket_strerror($socket_last_error),
+                ),
+            );
+        }
+
         return new self(
             $socket,
         );
@@ -33,7 +56,8 @@ final class Connection
     public function bind(): void
     {
         $n = random_int(0, PHP_INT_MAX);
-        $file = sprintf(self::DEFAULT_SOCKET_URL, $n);
+        $file = sprintf(sys_get_temp_dir() . '/yii-dev-server-%d.sock', $n);
+        $this->uri = $file;
         if (!socket_bind($this->socket, $file)) {
             $socket_last_error = socket_last_error($this->socket);
 
@@ -47,25 +71,65 @@ final class Connection
         }
     }
 
-    public function broadcast(string $data): void
+    /**
+     * @return Generator<int, array{0: self::TYPE_RESULT|self::TYPE_ERROR, 1: string, 2: string|int, 3?: int}>
+     */
+    public function read(): Generator
     {
-        $files = glob(self::DEFAULT_SOCKET_DIR . '-*.sock', GLOB_NOSORT);
+        while (true) {
+            if (!socket_recvfrom($this->socket, $buffer, self::DEFAULT_BUFFER_SIZE, MSG_DONTWAIT, $ip, $port)) {
+                $socket_last_error = socket_last_error($this->socket);
+                if ($socket_last_error === 35) {
+                    usleep(self::DEFAULT_TIMEOUT);
+                    continue;
+                }
+                $this->close();
+                yield [self::TYPE_ERROR, $socket_last_error, socket_strerror($socket_last_error)];
+                continue;
+            }
+            yield [self::TYPE_RESULT, $buffer, $ip, $port];
+        }
+    }
+
+    public function broadcast(int $type, string $data): void
+    {
+        $files = glob(sys_get_temp_dir() . '/yii-dev-server-*.sock', GLOB_NOSORT);
         //echo 'Files: ' . implode(', ', $files) . "\n";
+        $uniqueErrors = [];
+        $payload = json_encode([$type, $data]);
+        $payloadLength = strlen($payload);
         foreach ($files as $file) {
-            $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
-            if (!@socket_connect($socket, $file)) {
+            $socket = @fsockopen('udg://' . $file, -1, $errno, $errstr);
+            if ($errno === 61) {
                 @unlink($file);
                 continue;
             }
+            if ($errno !== 0) {
+                $uniqueErrors[$errno] = $errstr;
+                continue;
+            }
             try {
-                socket_send($socket, $data, strlen($data), 0);
+                if (!@fwrite($socket, $payload, $payloadLength)) {
+                    $err = socket_last_error($socket);
+                    $uniqueErrors[$err] =  socket_strerror($err);
+                    /**
+                     * Connection is closed.
+                     */
+                    continue;
+                }
             } catch (Throwable $e) {
-                unlink($file);
+                //@unlink($file);
                 throw $e;
             } finally {
-                socket_close($socket);
+                fflush($socket);
+                fclose($socket);
             }
         }
+    }
+
+    public function getUri(): string
+    {
+        return $this->uri;
     }
 
     public function close(): void
@@ -73,30 +137,5 @@ final class Connection
         @socket_getsockname($this->socket, $path);
         @socket_close($this->socket);
         @unlink($path);
-    }
-
-    public function read(Closure $onSuccess, ?Closure $onError = null): \Generator
-    {
-        while (true) {
-            if (!socket_recvfrom($this->socket, $buffer, 32768, MSG_DONTWAIT, $ip, $port)) {
-                $socket_last_error = socket_last_error($this->socket);
-                if ($socket_last_error === 35) {
-                    continue;
-                }
-                $this->close();
-                if ($onError !== null) {
-                    yield $onError($socket_last_error);
-                    break;
-                }
-                throw new RuntimeException(
-                    sprintf(
-                        'An error occurred while reading the socket. socket_last_error returned %d: "%s".',
-                        $socket_last_error,
-                        socket_strerror($socket_last_error)
-                    ),
-                );
-            }
-            yield $onSuccess($buffer, $ip, $port);
-        }
     }
 }
