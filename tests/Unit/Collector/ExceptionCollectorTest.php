23dce698e6c53c7dfaebf44e0db74744407146e4
Dmitrii Derepko <xepozz@list.ru>
2023-10-13 19:32:08 +0300

    Cover more cases

--- tests/Unit/Collector/ExceptionCollectorTest.php
+++ tests/Unit/Collector/ExceptionCollectorTest.php
@@ -18,7 +18,7 @@ final class ExceptionCollectorTest extends AbstractCollectorTestCase
      */
     protected function collectTestData(CollectorInterface $collector): void
     {
-        $exception = new Exception('test', 777);
+        $exception = new Exception('test', 777, new Exception('previous', 666));
         $collector->collect(new ApplicationError($exception));
     }
 
@@ -30,20 +30,26 @@ final class ExceptionCollectorTest extends AbstractCollectorTestCase
     protected function checkCollectedData(array $data): void
     {
         parent::checkCollectedData($data);
-        $this->assertCount(1, $data);
+        $this->assertCount(2, $data);
+        foreach ($data as $exception) {
+            $this->assertArrayHasKey('class', $exception);
+            $this->assertArrayHasKey('message', $exception);
+            $this->assertArrayHasKey('file', $exception);
+            $this->assertArrayHasKey('line', $exception);
+            $this->assertArrayHasKey('code', $exception);
+            $this->assertArrayHasKey('trace', $exception);
+            $this->assertArrayHasKey('traceAsString', $exception);
+        }
+
         $exception = $data[0];
-
-        $this->assertArrayHasKey('class', $exception);
-        $this->assertArrayHasKey('message', $exception);
-        $this->assertArrayHasKey('file', $exception);
-        $this->assertArrayHasKey('line', $exception);
-        $this->assertArrayHasKey('code', $exception);
-        $this->assertArrayHasKey('trace', $exception);
-        $this->assertArrayHasKey('traceAsString', $exception);
-
         $this->assertEquals(Exception::class, $exception['class']);
         $this->assertEquals('test', $exception['message']);
         $this->assertEquals(777, $exception['code']);
+
+        $exception = $data[1];
+        $this->assertEquals(Exception::class, $exception['class']);
+        $this->assertEquals('previous', $exception['message']);
+        $this->assertEquals(666, $exception['code']);
     }
 
     protected function checkSummaryData(array $data): void
--- tests/Unit/Collector/HttpClientCollectorTest.php
+++ tests/Unit/Collector/HttpClientCollectorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Debug\Tests\Unit\Collector;
 
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Yiisoft\Yii\Debug\Collector\CollectorInterface;
 use Yiisoft\Yii\Debug\Collector\HttpClientCollector;
@@ -18,16 +19,22 @@ final class HttpClientCollectorTest extends AbstractCollectorTestCase
     protected function collectTestData(CollectorInterface $collector): void
     {
         $collector->collect(
-            new \GuzzleHttp\Psr7\Request('GET', 'http://example.com'),
+            new Request('GET', 'http://example.com'),
             startTime: 10.10,
             line: 'file1:123',
             uniqueId: 'test1',
         );
         $collector->collect(
-            new \GuzzleHttp\Psr7\Request('GET', 'http://yiiframework.com'),
+            new Request('POST', 'http://yiiframework.com'),
             startTime: 12.10,
             line: 'file2:555',
             uniqueId: 'test2'
+        );
+        $collector->collect(
+            new Request('GET', 'http://yiiframework.com'),
+            startTime: 15.00,
+            line: 'file2:666',
+            uniqueId: 'test3'
         );
 
         $collector->collectTotalTime(
@@ -40,6 +47,11 @@ final class HttpClientCollectorTest extends AbstractCollectorTestCase
             endTime: 12.20,
             uniqueId: 'test2'
         );
+        $collector->collectTotalTime(
+            new Response(200, [], 'test'),
+            endTime: 20.00,
+            uniqueId: 'test4'
+        );
     }
 
     protected function getCollector(): CollectorInterface
@@ -51,7 +63,34 @@ final class HttpClientCollectorTest extends AbstractCollectorTestCase
     {
         parent::checkCollectedData($data);
 
-        $this->assertCount(2, $data);
+        $this->assertCount(3, $data);
+
+        $entry = $data[0];
+        $this->assertEquals(10.10, $entry['startTime']);
+        $this->assertEquals(13.10, $entry['endTime']);
+        $this->assertEquals(3.0, $entry['totalTime']);
+        $this->assertEquals('GET', $entry['method']);
+        $this->assertEquals('http://example.com', $entry['uri']);
+        $this->assertEquals(['Host' => ['example.com']], $entry['headers']);
+        $this->assertEquals('file1:123', $entry['line']);
+
+        $entry = $data[1];
+        $this->assertEquals(12.10, $entry['startTime']);
+        $this->assertEquals(12.20, $entry['endTime']);
+        $this->assertEquals(0.1, round($entry['totalTime'], 1));
+        $this->assertEquals('POST', $entry['method']);
+        $this->assertEquals('http://yiiframework.com', $entry['uri']);
+        $this->assertEquals(['Host' => ['yiiframework.com']], $entry['headers']);
+        $this->assertEquals('file2:555', $entry['line']);
+
+        $entry = $data[2];
+        $this->assertEquals(15.0, $entry['startTime']);
+        $this->assertEquals(15.0, $entry['endTime']);
+        $this->assertEquals(0.0, round($entry['totalTime'], 1));
+        $this->assertEquals('GET', $entry['method']);
+        $this->assertEquals('http://yiiframework.com', $entry['uri']);
+        $this->assertEquals(['Host' => ['yiiframework.com']], $entry['headers']);
+        $this->assertEquals('file2:666', $entry['line']);
     }
 
     protected function checkSummaryData(array $data): void
@@ -63,7 +102,7 @@ final class HttpClientCollectorTest extends AbstractCollectorTestCase
         $this->assertArrayHasKey('count', $data['http']);
         $this->assertArrayHasKey('totalTime', $data['http']);
 
-        $this->assertEquals(2, $data['http']['count']);
+        $this->assertEquals(3, $data['http']['count']);
         $this->assertEquals(3.1, round($data['http']['totalTime'], 1));
     }
 }
