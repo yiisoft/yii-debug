a679425b9afd2ae784362e444c3914e16b911768
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-06-30 21:36:23 +0300

    Finalize `StorageInterface` constants + Fix tests (#260)

--- tests/Unit/DebugServiceProviderTest.php
+++ tests/Unit/DebugServiceProviderTest.php
@@ -5,34 +5,21 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Debug\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\EventDispatcher\ListenerProviderInterface;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
+use Psr\Container\ContainerInterface;
 use Yiisoft\Di\Container;
 use Yiisoft\Di\ContainerConfig;
-use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
-use Yiisoft\EventDispatcher\Provider\Provider;
+use Yiisoft\Yii\Debug\Collector\ContainerInterfaceProxy;
 use Yiisoft\Yii\Debug\DebugServiceProvider;
-use Yiisoft\Yii\Debug\Collector\EventDispatcherInterfaceProxy;
-use Yiisoft\Yii\Debug\Collector\LoggerInterfaceProxy;
 
 final class DebugServiceProviderTest extends TestCase
 {
     public function testRegister(): void
     {
-        $config = ContainerConfig::create()
-            ->withDefinitions([
-                LoggerInterface::class => NullLogger::class,
-                EventDispatcherInterface::class => Dispatcher::class,
-                ListenerProviderInterface::class => Provider::class,
-            ])
-            ->withProviders([
-                new DebugServiceProvider(),
-            ]);
+        $config = ContainerConfig::create()->withProviders([
+            new DebugServiceProvider(),
+        ]);
         $container = new Container($config);
 
-        $this->assertInstanceOf(LoggerInterfaceProxy::class, $container->get(LoggerInterface::class));
-        $this->assertInstanceOf(EventDispatcherInterfaceProxy::class, $container->get(EventDispatcherInterface::class));
+        $this->assertInstanceOf(ContainerInterfaceProxy::class, $container->get(ContainerInterface::class));
     }
 }
