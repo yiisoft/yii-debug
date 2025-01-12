863158f023ff9a696241de70950c6a0bd3255978
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-01-12 18:57:50 +0300

    Remove `DebuggerIdGenerator`.

--- tests/Unit/Command/ResetCommandTest.php
+++ tests/Unit/Command/ResetCommandTest.php
@@ -8,17 +8,15 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Yiisoft\Yii\Debug\Command\DebugResetCommand;
 use Yiisoft\Yii\Debug\Debugger;
-use Yiisoft\Yii\Debug\DebuggerIdGenerator;
 use Yiisoft\Yii\Debug\Storage\StorageInterface;
 
 final class ResetCommandTest extends TestCase
 {
     public function testCommand(): void
     {
-        $idGenerator = new DebuggerIdGenerator();
         $storage = $this->createMock(StorageInterface::class);
         $storage->expects($this->once())->method('clear');
-        $debugger = new Debugger($idGenerator, $storage, []);
+        $debugger = new Debugger($storage, []);
 
         $command = new DebugResetCommand($storage, $debugger);
 
