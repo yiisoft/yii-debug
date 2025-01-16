557e78d861ccac84ef07e08d733158a2036aa51c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-01-16 22:04:33 +0300

    Add `PredefinedConditionTest`

---
+++ tests/Unit/StartupPolicy/Condition/PredefinedConditionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\StartupPolicy\Condition;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Yiisoft\Yii\Debug\StartupPolicy\Condition\PredefinedCondition;
+
+final class PredefinedConditionTest extends TestCase
+{
+    public static function dataBase(): iterable
+    {
+        yield 'true' => [true];
+        yield 'false' => [false];
+    }
+
+    #[DataProvider('dataBase')]
+    public function testBase(bool $value): void
+    {
+        $event = new stdClass();
+        $condition = new PredefinedCondition($value);
+
+        $this->assertSame($value, $condition->match($event));
+    }
+}
