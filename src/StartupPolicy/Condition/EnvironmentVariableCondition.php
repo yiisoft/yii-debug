e638d42a42742be0fcb7de7ab5d78e02d197536c
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-01-16 22:13:26 +0300

    Add `EnvironmentVariableCondition`

--- src/StartupPolicy/Condition/EnvironmentVariableCondition.php
+++ src/StartupPolicy/Condition/EnvironmentVariableCondition.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Debug\StartupPolicy\Condition;
 
+use function in_array;
+use function is_string;
+
 final class EnvironmentVariableCondition implements ConditionInterface
 {
+    private const TRUE_VALUES = ['1', 'true', 'on'];
+
     public function __construct(
         private readonly string $variableName,
     ) {
@@ -13,6 +18,11 @@ final class EnvironmentVariableCondition implements ConditionInterface
 
     public function match(object $event): bool
     {
-        return (bool) getenv($this->variableName);
+        $value = getenv($this->variableName);
+        if (!is_string($value)) {
+            return false;
+        }
+
+        return in_array(strtolower($value), self::TRUE_VALUES, true);
     }
 }
---
+++ tests/Unit/StartupPolicy/Condition/EnvironmentVariableConditionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\StartupPolicy\Condition;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Yiisoft\Yii\Debug\StartupPolicy\Condition\EnvironmentVariableCondition;
+
+final class EnvironmentVariableConditionTest extends TestCase
+{
+    public static function dataBase(): iterable
+    {
+        yield 'true' => [true, 'true'];
+        yield 'false' => [false, 'false'];
+        yield 'on' => [true, 'on'];
+        yield 'off' => [false, 'off'];
+        yield 'one' => [true, '1'];
+        yield 'zero' => [false, '0'];
+        yield 'empty-string' => [false, ''];
+        yield 'custom-string' => [false, 'test'];
+    }
+
+    #[DataProvider('dataBase')]
+    public function testBase(bool $expected, string $value): void
+    {
+        $variableName = 'YII_DEBUG_TEST_ENVIRONMENT_VARIABLE';
+        $event = new stdClass();
+        $condition = new EnvironmentVariableCondition($variableName);
+
+        putenv("$variableName=$value");
+
+        $this->assertSame($expected, $condition->match($event));
+    }
+}
